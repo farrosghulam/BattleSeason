d0e04e02a0d91621ae91d5a85ec1af4da080cfb2
farrosghulam <farrosghulam@gmail.com>
2026-09-23 14:56:45 +0700

    adding loop and finishing

--- Program.cs
+++ Program.cs
@@ -11,3 +11,14 @@ battleNumbers = Console.ReadLine();
 Console.WriteLine("Input the EXP gained per battle: ");
 EXP = Console.ReadLine();
 
+Console.WriteLine("\n----------------------------\n");
+
+for (int i = 1; i <= int.Parse(battleNumbers); i++)
+{
+    Console.WriteLine($"Battle {i} | EXP gained: {EXP} | Total EXP: {int.Parse(EXP) * i}");
+}
+
+Console.WriteLine("\n === BATTLE COMPLETE === ");
+Console.WriteLine("Character : " + characterName);
+Console.WriteLine("Battles: " + battleNumbers);
+Console.WriteLine("Total EXP: " + (int.Parse(EXP) * int.Parse(battleNumbers)));
