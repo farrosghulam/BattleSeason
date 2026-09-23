848ca206d5171de65a125a1cdcd83f36dc4ea8f7
farrosghulam <farrosghulam@gmail.com>
2026-09-23 14:43:15 +0700

    adding the other input thingy

--- Program.cs
+++ Program.cs
@@ -5,3 +5,9 @@ string EXP;
 Console.WriteLine("Input character's name:");
 characterName = Console.ReadLine();
 
+Console.WriteLine("Input the number of battles: ");
+battleNumbers = Console.ReadLine();
+
+Console.WriteLine("Input the EXP gained per battle: ");
+EXP = Console.ReadLine();
+
